4e00d37bb1f1cbf49b31c482c63bdc16b786f1a4
Francis Lim <lim04francis@gmail.com>
2020-12-16 14:11:22 -0800

    static/style

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def home():
     #function use Flask import (Jinja) to render an HTML template
     return render_template("home.html")
 
+
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True, port='3000', host='127.0.0.1')
