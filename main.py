c2fb75378bb6f7d33f00dd5e635f80d83d9907da
Malama <mvvillah@icloud.com>
2020-12-18 13:43:37 -0800

    Started working on the movies page for the website, it is not final, just experimenting

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def home():
 @app.route('/cubism')
 def cubism():
     return render_template("cubism.html")
+
+@app.route('/Movies')
+def Movies():
+    return render_template("Movies.html")
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True, port='3000', host='127.0.0.1')
