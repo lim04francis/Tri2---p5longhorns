8cbd5e37e9b6d8f09b83f3ef602275e9b1fc3c1c
Malama <mvvillah@icloud.com>
2021-01-07 13:42:07 -0800

    Created Music Page and added post functioning search bar thing thats not fully finished yet

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 # https://flask.palletsprojects.com/en/1.1.x/api/
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 #create a Flask instance
 app = Flask(__name__)
 
@@ -23,6 +23,24 @@ def photography():
 @app.route('/Movies')
 def Movies():
     return render_template("Movies.html")
+
+@app.route('/music')
+def Music():
+    return render_template('Music.html', genremain="")
+
+
+@app.route("/Genresearch1", methods=["GET", "POST"])
+def genresearch():
+    if request.method == "POST":
+        form= request.form
+        Genre1 = form['MusicG1']
+        return render_template('/music.html', genremain=Genre1)
+
+
+
+
+
+
 if __name__ == "__main__":
     #runs the application on the repl development server
     app.run(debug=True, port='3000', host='127.0.0.1')
