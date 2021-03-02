3882d1842f2ed5fc34a412154941b0076e0e581d
marchumeau <72947138+marchumeau@users.noreply.github.com>
2021-03-01 16:16:03 -0800

    Added more paintings into the dictionaries and the lists for the table loop.

--- paintings.py
+++ paintings.py
@@ -12,6 +12,20 @@ p8 = {"name":"Philosopher's Lamp", "date": 1936, "painter":"Rene Margritte", "ge
 p9 = {"name":"The Lovers", "date": 1928, "painter":"Rene Margritte", "genre":"Surrealism"}
 p10 = {"name":"Golconda", "date": 1953, "painter":"Rene Margritte", "genre":"Surrealism"}
 
-painting_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10]
+#Abstract
+p11 = {"name":"Composition VII", "date": 1913, "painter":"Wassily Kandinsky", "genre":"Abstract"}
+p12 = {"name":"Convergence", "date": 1952, "painter":"Jackson Pollock", "genre":"Abstract"}
+p13 = {"name":"Composition with Red, Blue and Yellow", "date": 1930, "painter":"Piet Mondrian", "genre":"Abstract"}
+p14 = {"name":"Based on Leaf Forms and Spaces", "date": 1912, "painter":"Arthur Dove", "genre":"Abstract"}
+p15 = {"name":"Black Square", "date": 1915, "painter":"Kazimir Malevich", "genre":"Abstract"}
+
+#Romanticism
+p16 = {"name":"The Desperate Man", "date": 1845, "painter":"Gustave Courbet", "genre":"Romanticism"}
+p17 = {"name":"Wanderer above the Sea of Fog", "date": 1818, "painter":"Caspar David Friedrich", "genre":"Romanticism"}
+p18 = {"name":"Liberty Leading the People", "date": 1830, "painter":"Eugène Delacroix", "genre":"Romanticism"}
+p19 = {"name":"The Soul of the Rose", "date": 1908, "painter":"John William Waterhouse", "genre":"Romanticism"}
+p20 = {"name":"Saturn Devouring His Son", "date": 1823, "painter":"Francisco Goya", "genre":"Romanticism"}
+
+painting_list = [p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20]
 
 
